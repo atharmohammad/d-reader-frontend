a7f5c2168557ade01a0792a98e1e912d458bd2df
Athar <mohd.rule123@gmail.com>
2023-02-11 16:56:57 +0530

    adding remaning accounts for sol payment guards

--- pages/_index.tsx
+++ pages/_index.tsx
@@ -37,11 +37,13 @@ function MintComponent() {
         if(!provider || !wallet)return;
         const program  = new Program(idlJSON, idl.metadata.address, provider);
         const mint = Keypair.generate();
-        const candyMachine = new PublicKey('B7BnAA15YqwX6YkqmBD5ktbH8qwQVTzTZ3km5FLv7tkY');
-        // const key =  Keypair.fromSecretKey(
-        //     new Uint8Array([209,51,22,116,112,76,53,187,35,100,116,37,253,206,248,191,90,235,45,204,192,66,204,174,95,120,45,20,110,52,243,86,90,150,196,14,140,0,201,105,169,208,130,240,117,171,205,201,241,105,26,234,14,213,128,26,147,60,103,98,136,177,251,66]),
-        //   );
-        const mint_ix = await mintInstruction(candyMachine,wallet,mint,provider.connection,undefined,undefined,"Group 1");
+        const candyMachine = new PublicKey('2cMgvNiVs8ULqm482uEfHjWT8kCVg7e8JdENGF4ahDHN');
+        const destination = new PublicKey("3ZQgnUCPVYufuMycRhjRp7UaoBwqzYYm87jGeMSS7NMo");
+        const mint_ix = await mintInstruction(candyMachine,wallet,mint,provider.connection,[{
+            pubkey:destination,
+            isSigner:false,
+            isWritable:true
+        }]);
         if(!mint_ix){
             return;
         }
